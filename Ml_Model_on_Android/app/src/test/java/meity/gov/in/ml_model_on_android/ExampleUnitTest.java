bfe3db3676ed67cd8cc67dabaa1d36f9f38d5f59
Satya Prakash <satyaprakas5555@gmail.com>
2022-06-03 05:22:04 -0700

    First Commit

---
+++ Ml_Model_on_Android/app/src/test/java/meity/gov/in/ml_model_on_android/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package meity.gov.in.ml_model_on_android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
